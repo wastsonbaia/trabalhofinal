68c015d2347d496a700dbb4a5ec633cad59d46fb
wastsonbaia <51840364+wastsonbaia@users.noreply.github.com>
2019-06-23 20:21:28 -0300

    Update webwp.sh

--- webwp.sh
+++ webwp.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
-apt-get update 
-apt-get install mysql-client php7.2 php7.2-mysql libapache2-mod-php7.2 php7.2-cli php7.2-cgi php7.2-gd apache2 apache2-utils -y 
-systemctl enable apache2 
-systemctl start apache2
-sudo apt install curl -y
+
+#INSTALA O O QUE É NECESSÁRIO
+sudo apt -y update
+sudo apt -y install php-curl php-gd php-mbstring php-xml php-xmlrpc
+sudo apt-get -y install mysql-server
+sudo apt -y install apache2
+sudo apt -y install php libapache2-mod-php php-mysql
+sudo apt install curl
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
-mv wp-config-sample.php wp-config.php
+
+#CONFIGURA MYSQL PRA RECEBER O WORDPRESS
+sudo mysql <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+FLUSH PRIVILEGES;
+EOF
+
+#ALGUMAS MODIFICAÇÕES NO APACHE2
+
+sudo sed -i "s/Options Indexes FollowSymLinks/Options FollowSymLinks/" /etc/apache2/apache2.conf
+sudo a2dissite 000-default.conf
+sudo systemctl stop apache2.service
+sudo systemctl start apache2.service
+sudo systemctl enable apache2.service
+sudo su
 cd /var/www/html
-sudo chown -R :www-data /var/www/html/
+sudo mkdir wordpress
+cd /var/www/html/wordpress
+sudo chown -R `whoami`:www-data /var/www/html/wordpress
 wp core download --locale=pt_BR
+#CONFIGURANDO BD DO WORDPRESS
+sudo mv wp-config-sample.php wp-config.php
+
 sed -i 's/database_name_here/wordpress/g' wp-config.php
-sed -i 's/username_here/root/g' wp-config.php
-sed -i 's/password_here/wordpress/g' wp-config.php
+sed -i 's/username_here/admin/g' wp-config.php
+sed -i 's/password_here/root/g' wp-config.php
 sed -i "s/localhost/$IP_PRIVATE_BD/g" wp-config.php
-
-systemctl restart apache2.service
-rm -rf /var/www/html/index.html
-sed -i '/warn/a <Directory /var/www/html/>\n   AllowOverride All\n</Directory>' /etc/apache2/sites-available/000-default.conf
-a2enmod rewrite
-service apache2 restart
-touch /var/www/html/.htaccess
-chown :www-data /var/www/html/.htaccess
-chmod 664 /var/www/html/.htaccess
-
-wp core install --url=http://$IP_PRIVATE_BD --title=Blog\ Cozinha --admin_user=admin --admin_password=123456 --admin_email=teste@teste.com.br
-service apache2 restart
+IP_Public="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)" 
+wp core install --url=http://IP_Public/d --title=Blog\ Cozinha --admin_user=admin --admin_password=123456 --admin_email=teste@teste.com.br
