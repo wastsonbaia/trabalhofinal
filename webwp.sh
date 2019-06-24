07898fc21369dcaf84f930413700a1309bed4586
wastsonbaia <51840364+wastsonbaia@users.noreply.github.com>
2019-06-24 20:26:22 -0300

    Update webwp.sh

--- webwp.sh
+++ webwp.sh
@@ -1,43 +1,67 @@
 #!/bin/bash
-
-#INSTALA O O QUE É NECESSÁRIO
+#ATUALIZANDO E INSTALANDO AS COISAS
 sudo apt -y update
-sudo apt -y install php-curl php-gd php-mbstring php-xml php-xmlrpc
-sudo apt-get -y install mysql-server
-sudo apt -y install apache2
-sudo apt -y install php libapache2-mod-php php-mysql
-sudo apt install curl
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
-
-#CONFIGURA MYSQL PRA RECEBER O WORDPRESS
-sudo mysql <<EOF
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-
-FLUSH PRIVILEGES;
-EOF
-
-#ALGUMAS MODIFICAÇÕES NO APACHE2
-
+sudo apt -y install php-curl php-gd php-mbstring php-xml php-xmlrpc apache2 php libapache2-mod-php php-mysql
+#MODIFICAÇÕES NO APACHE
 sudo sed -i "s/Options Indexes FollowSymLinks/Options FollowSymLinks/" /etc/apache2/apache2.conf
 sudo a2dissite 000-default.conf
 sudo systemctl stop apache2.service
 sudo systemctl start apache2.service
 sudo systemctl enable apache2.service
-sudo su
-cd /var/www/html
-sudo mkdir wordpress
-cd /var/www/html/wordpress
-sudo chown -R `whoami`:www-data /var/www/html/wordpress
-wp core download --locale=pt_BR
+#INSTALANDO WORDPRESS
+ip_banco="${IPBD}"
+usuario="${USER}"
+senha="${PASS}"
+wget https://wordpress.org/latest.tar.gz
+tar -zxvf latest.tar.gz
+sudo mv wordpress /var/www/html/wordpress
+sudo chown -R www-data:www-data /var/www/html/wordpress/
+sudo chmod -R 755 /var/www/html/wordpress/
+cat <<EOF > /etc/apache2/sites-available/wordpress.conf
+<VirtualHost *:80>
+   ServerAdmin admin@example.com
+   DocumentRoot /var/www/html/wordpress/
+   ServerName example.com
+   ServerAlias www.example.com
+   <Directory /var/www/html/wordpress/> 
+        Options +FollowSymlinks
+        AllowOverride All
+        Require all granted
+   </Directory>
+   ErrorLog ${APACHE_LOG_DIR}/error.log
+   CustomLog ${APACHE_LOG_DIR}/access.log combined 
+</VirtualHost>
+EOF
+sudo a2ensite wordpress.conf
+sudo a2enmod rewrite
+sudo systemctl restart apache2
 #CONFIGURANDO BD DO WORDPRESS
-sudo mv wp-config-sample.php wp-config.php
-
-sed -i 's/database_name_here/wordpress/g' wp-config.php
-sed -i 's/username_here/admin/g' wp-config.php
-sed -i 's/password_here/root/g' wp-config.php
-sed -i "s/localhost/$IP_PRIVATE_BD/g" wp-config.php
-IP_Public="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)" 
-wp core install --url=http://IP_Public/d --title=Blog\ Cozinha --admin_user=admin --admin_password=123456 --admin_email=teste@teste.com.br
+sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+sudo sed -i "s/database_name_here/wordpress/g" /var/www/html/wordpress/wp-config.php
+sudo sed -i "s/username_here/wp_admin/g" /var/www/html/wordpress/wp-config.php 
+sudo sed -i "s/password_here/root/g" /var/www/html/wordpress/wp-config.php
+sudo sed -i "s/localhost/$ip_banco/g" /var/www/html/wordpress/wp-config.php
+## ALTERANDO A LINGUAGEM PARA PORTUGUES BRASILEIRO
+sudo sed -i "s/\$language = ''/\$language = 'pt_BR'/" /var/www/html/wordpress/wp-admin/install.php
+sudo echo "define('WPLANG', 'pt_BR');" >> /var/www/html/wordpress/wp-config.php
+wget https://github.com/eduardaviriato/final/blob/master/languages.tar.gz
+tar -zxf languages.tar.gz
+rm languages.tar.gz
+sudo mv languages /var/www/html/wordpress/wp-content/languages
+## ALTERANDO A VARIAVEL STEP PARA PASSAR IR PARA A TELA DE LOGIN
+n=`grep -n '$step =' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d :`
+sed -i ""$n"s/: 0;/: 2;/" /var/www/html/wordpress/wp-admin/install.php
+## ALTERANDO AS CONFIGURACOES DO USUÁRIO
+aux=`grep -n '$weblog_title[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d ":"`
+n=`echo $aux | cut -f 2 -d " "`
+sed -i ""$n"s/: '';/: 'Duda';/" /var/www/html/wordpress/wp-admin/install.php
+aux=`grep -n '$user_name[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d ":"`
+n=`echo $aux | cut -f 2 -d " "`
+sed -i ""$n"s/: '';/: '"$usuario"';/" /var/www/html/wordpress/wp-admin/install.php
+n=`grep -n '$admin_password[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d :`
+sed -i ""$n"s/: '';/: '"$senha"';/" /var/www/html/wordpress/wp-admin/install.php
+n=`grep -n '$admin_password_check[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d :`
+sed -i ""$n"s/: '';/: '"$senha"';/" /var/www/html/wordpress/wp-admin/install.php
+aux=`grep -n '$admin_email[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d ":"`
+n=`echo $aux | cut -f 2 -d " "`
+sed -i ""$n"s/: '';/: 'admin@email.com';/" /var/www/html/wordpress/wp-admin/install.php
